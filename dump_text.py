fc6fd63bf8de0300a562f3e7ad161892b1a4d9ac
Lee0701 <hgl1002@naver.com>
2024-10-11 18:37:21 +0900

    Add text dumper

---
+++ dump_text.py
@@ -0,0 +1,53 @@
+
+import sys
+
+def lst2int(lst):
+    result = 0
+    for digit in lst:
+        result = (result << 8) | digit
+    return result
+
+def main(args):
+    tbl_file, input_file, output_file = args
+    with open(tbl_file, 'r') as f:
+        tbl_content = f.readlines()
+    tbl_content = [line.replace('\n', '').replace('\r', '') for line in tbl_content]
+    tbl_content = [line.split('=') for line in tbl_content]
+    tbl_content = [entry for entry in tbl_content if len(entry) == 2]
+    tbl_content = [(int(key, 16), value) for key, value in tbl_content]
+    tbl_content = {key: value for key, value in tbl_content}
+
+    with open(input_file, 'rb') as f:
+        content = f.read()
+
+    result = []
+
+    offset = 0
+    while offset < len(content):
+        for k in reversed(range(1, 4)):
+            key = content[offset:offset+k]
+            key = lst2int(key)
+            if key in tbl_content:
+                value = offset + 1, tbl_content[key]
+                result.append(value)
+                offset += k
+                break
+        else:
+            value = offset + 1, f'<{content[offset]:02x}>'
+            result.append(value)
+            offset += 1
+
+    with open(output_file, 'w') as f:
+        f.write(f'{0:06x}\t')
+        for offset, token in enumerate(result):
+            offset, token = token
+            f.write(token)
+            if token == '<end>':
+                f.write('\n')
+                if offset < len(content) - 1:
+                    f.write(f'{offset:06x}\t')
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
