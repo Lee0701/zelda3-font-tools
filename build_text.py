a02660af46b6fa7ed883e2e27ab8f8e5fb19f6b6
Lee0701 <hgl1002@naver.com>
2024-10-13 01:33:25 +0900

    Add base text support for build text script

--- build_text.py
+++ build_text.py
@@ -12,7 +12,7 @@ def int2lst(num):
     return result[::-1]
 
 def main(args):
-    tbl_file, input_file, output_file = args
+    tbl_file, base_text_file, text_file, output_file = args
     with open(tbl_file, 'r') as f:
         tbl_content = f.readlines()
     tbl_content = [line.replace('\n', '').replace('\r', '') for line in tbl_content]
@@ -22,16 +22,25 @@ def main(args):
     tbl_content = {value: key for key, value in tbl_content}
     max_key_len = max(len(key) for key in tbl_content.keys()) + 1
 
-    content = ''
+    with open(base_text_file, 'r') as f:
+        base_content = yaml.load(f, yaml.SafeLoader)
+        base_content = base_content.values()
 
-    with open(input_file, 'r') as f:
-        content = yaml.load(f, yaml.SafeLoader)
-    content = content.values()
-    content = [line + '<end>' for line in content]
+    with open(text_file, 'r') as f:
+        new_content = yaml.load(f, yaml.SafeLoader)
+        new_content = new_content.values()
+
+    end_token = '<end>'
+    content = []
+    for base, line in zip(base_content, new_content):
+        if line == '':
+            content.append(base)
+        else:
+            content.append(line)
+    content = [line + end_token for line in content]
     content = ''.join(content)
 
     result = []
-
     offset = 0
     while offset < len(content):
         for k in reversed(range(1, max_key_len)):
